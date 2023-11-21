0cebae29039b8f65f4e3f35650be9473f5be95cd
irfanali2504 <ir.irfan106@gmail.com>
2023-11-21 11:40:31 +0530

    contact us page

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -7,6 +7,9 @@ const AboutPage = () => {
     maxWidth: 'md',
     marginTop: '4rem',
     marginBottom: '4rem',
+    paddingTop: '2rem', 
+    paddingBottom: '2rem', 
+    boxShadow: '14px 10px 50px 10px rgba(0, 0, 0, 0.5)'
   };
 
   const paperStyle = {
@@ -17,63 +20,64 @@ const AboutPage = () => {
 
   return (
     <>
-    <Box
-            sx={{
-                position: 'relative',
-                height: '300px',
-                overflow: 'hidden',
-            }}
+      <Box
+        sx={{
+          position: 'relative',
+          height: '300px',
+          overflow: 'hidden',
+        }}
+      >
+        <Box
+          component="div"
+          sx={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100%',
+            filter: 'blur(5px)',
+            borderBottom: '10px solid'
+          }}
+        />
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white',
+          }}
         >
-            <Box
-                component="div"
-                sx={{
-                    backgroundImage: `url(${backgroundImage})`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    height: '100%',
-                    filter: 'blur(5px)',
-                }}
-            />
-            <Box
-                sx={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    color: 'white',
-                }}
-            >
-                <Typography variant="h2">About Us</Typography>
-            </Box>
+          <Typography variant="h2">About Us</Typography>
         </Box>
-    <Container style={containerStyle}>
-      <Paper style={paperStyle}>
-        <Typography variant="body1" paragraph>
-          Welcome to DevNerd, your go-to platform for finding and applying to tech jobs! We understand the
-          unique needs of tech professionals and strive to connect talented individuals with exciting job
-          opportunities in the tech industry.
-        </Typography>
-        <Typography variant="body1" paragraph>
-          Our mission is to simplify the job search process for tech enthusiasts by providing a user-friendly
-          platform where they can discover and apply to their favorite jobs effortlessly. Whether you're a
-          software developer, data scientist, or IT professional, DevNerd has the right opportunities for you.
-        </Typography>
-        <Typography variant="body1" paragraph>
-          At DevNerd, we believe in the power of technology to transform lives and businesses. We're passionate
-          about connecting top talent with innovative companies, fostering growth and success in the tech
-          community.
-        </Typography>
-        <Typography variant="body1" paragraph>
-          Thank you for being a part of the DevNerd community. If you have any questions or feedback, feel free
-          to reach out to us. Happy job hunting!
-        </Typography>
-      </Paper>
-    </Container>
+      </Box>
+      <Container style={containerStyle}>
+        <Paper style={paperStyle}>
+          <Typography variant="body1" paragraph>
+            Welcome to DevNerd, your go-to platform for finding and applying to tech jobs! We understand the
+            unique needs of tech professionals and strive to connect talented individuals with exciting job
+            opportunities in the tech industry.
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Our mission is to simplify the job search process for tech enthusiasts by providing a user-friendly
+            platform where they can discover and apply to their favorite jobs effortlessly. Whether you're a
+            software developer, data scientist, or IT professional, DevNerd has the right opportunities for you.
+          </Typography>
+          <Typography variant="body1" paragraph>
+            At DevNerd, we believe in the power of technology to transform lives and businesses. We're passionate
+            about connecting top talent with innovative companies, fostering growth and success in the tech
+            community.
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Thank you for being a part of the DevNerd community. If you have any questions or feedback, feel free
+            to reach out to us. Happy job hunting!
+          </Typography>
+        </Paper>
+      </Container>
     </>
   );
 };
